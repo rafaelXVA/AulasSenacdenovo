f732a334908072eefe5f518aeecf85923e0d4a9c
RafaelXVA <rafael.rocha2745@gmail.com>
2025-02-28 18:37:41 -0300

    desktop

--- desktop/lan/main.py
+++ desktop/lan/main.py
@@ -4,6 +4,8 @@ from PySide6.QtGui import QPixmap, QIcon
 from form import Ui_furmulario
 from layout import Ui_MainWindow as Ui_Login
 from cardapio import Ui_MainWindow as Ui_Cardapio
+import mysql.connector
+
 
 class MainWindow(QMainWindow):
     
@@ -23,6 +25,7 @@ class MainWindow(QMainWindow):
         self.login.btn_login.clicked.connect(self.btn_login)
         self.login.btn_signup.clicked.connect(self.btn_signup)
 
+
     def btn_login(self):
         self.password=self.login.le_password.text()
         self.user=self.login.le_user.text()
@@ -39,6 +42,7 @@ class MainWindow(QMainWindow):
     def btn_signup(self):
         window.close()
         window_cadastro.show()
+    
 
 class Cadastro(QMainWindow):
 
@@ -49,6 +53,35 @@ class Cadastro(QMainWindow):
         self.cadastro.btn_completar.clicked.connect(self.completar)
     def completar(self):
         window_cadastro.close()
+        nome=self.cadastro.LE_nome.text()
+        cpf=self.cadastro.LE_cpf.text()
+        self.cadastro.R_masculino.setChecked(True)
+        sexo=int()
+        if self.cadastro.R_masculino.isChecked():
+            return sexo==1
+        elif self.cadastro.R_feminino.isChecked():
+            return sexo==2
+        endereco=self.cadastro.LE_endereco.text()
+        telefone=self.cadastro.LE_telefone.text()
+        nascimento=self.cadastro.DE_nascimento.date()
+        user=self.cadastro.LE_user.text()
+        senha=self.cadastro.LE_senha.text()
+
+        self.sql=mysql.connector.connect(
+            user='suporte2',
+            password='',
+            host='localhost',
+            database='lanchonete'
+        )
+        cursor=self.sql.cursor()
+        cursor.execute('insert into funcionario (id_funcionario, nome, cpf, sexo, endereco, telefone, nascimento, usuario, senha) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+               (nome, cpf, sexo, endereco, telefone, nascimento, user, senha))
+        self.sql.commit()
+        self.cadastro.LE_cpf.clear()
+        self.cadastro.LE_endereco.clear()
+        self.cadastro.LE_nome.clear()
+        self.cadastro.LE_senha.clear()
+        self.cadastro.LE_user.clear()
         window.show()
 class Cardapio(QMainWindow):
 
